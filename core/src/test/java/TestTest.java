dc86ee351453424f53bff209d4d06be51a6496b7
jonatan <32123499+jonaengs@users.noreply.github.com>
2021-03-10 21:17:41 +0100

    Project fixes (#20)
    Gradle:
    * Set java version to 1.8 and min android api version to 24
    * Upgrade Gradle version
    * Set distributiontype to all (meaning gradle will download sources and docs)
    
    Project:
    * Setup test structure
    * Delete readmes

---
+++ core/src/test/java/TestTest.java
@@ -0,0 +1,34 @@
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.XmlReader;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void ReadResourceTest() throws IOException {
+        String fp = this.getClass().getClassLoader().getResource("test.txt").getPath().substring(1);
+        // https://stackoverflow.com/questions/27886918/running-tests-in-libgdx-using-intellij
+        byte[] content = Gdx.files == null ?
+                Files.readAllBytes(Paths.get(fp))
+                : Gdx.files.internal(fp).readBytes();
+        assertTrue(new String(content).contentEquals("test"));
+    }
+
+    @Test
+    public void ReadXMLTest() throws IOException {
+        String fp = this.getClass().getClassLoader().getResource("test.xml").getPath().substring(1);
+        String content = new String(Files.readAllBytes(Paths.get(fp)), StandardCharsets.UTF_8);
+        XmlReader.Element element = new XmlReader().parse(content);
+        assertTrue(element.getText().contentEquals("test"));
+    }
+}
