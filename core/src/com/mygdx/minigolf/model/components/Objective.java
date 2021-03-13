99783b6932b847f10800a802485a00e2c60ea1a8
Ralf Leistad <leistad.ralf@gmail.com>
2021-03-13 01:07:08 +0100

    #6 Added basic for components

--- core/src/com/mygdx/minigolf/model/components/Objective.java
+++ core/src/com/mygdx/minigolf/model/components/Objective.java
@@ -1,4 +1,21 @@
 package com.mygdx.minigolf.model.components;
 
-public class Objective {
+import com.badlogic.ashley.core.Component;
+
+public class Objective implements Component {
+
+    private int par;
+
+
+    //
+    //  GETTERS AND SETTERS
+    //
+
+    public int getPar() {
+        return par;
+    }
+
+    public void setPar(int par) {
+        this.par = par;
+    }
 }
--- core/src/com/mygdx/minigolf/model/components/Player.java
+++ core/src/com/mygdx/minigolf/model/components/Player.java
@@ -1,4 +1,21 @@
 package com.mygdx.minigolf.model.components;
 
-public class Player {
+import com.badlogic.ashley.core.Component;
+
+public class Player implements Component {
+
+    private int strokes = 0;
+
+
+    //
+    //  GETTERS AND SETTERS
+    //
+
+    public int getStrokes() {
+        return strokes;
+    }
+
+    public void setStrokes(int strokes) {
+        this.strokes = strokes;
+    }
 }
