e7fa4f4a82b7918b4b80871f21eb04635917cb5a
jonatan <jonaengs@gmail.com>
2021-03-16 13:19:02 +0100

    remove comment, use iterators for ObjectiveSystem update loop

--- core/src/com/mygdx/minigolf/controller/systems/ObjectiveSystem.java
+++ core/src/com/mygdx/minigolf/controller/systems/ObjectiveSystem.java
@@ -11,7 +11,6 @@ import com.mygdx.minigolf.model.components.Objective;
 import com.mygdx.minigolf.model.components.Physical;
 import com.mygdx.minigolf.model.components.Player;
 
-// All logic should go in the system
 public class ObjectiveSystem extends EntitySystem {
     private ImmutableArray<Entity> objectives;
     private ImmutableArray<Entity> players;
@@ -21,20 +20,19 @@ public class ObjectiveSystem extends EntitySystem {
     private final ComponentMapper<Player> playerMapper = ComponentMapper.getFor(Player.class);
 
     public void addedToEngine(Engine engine) {
-        objectives = engine.getEntitiesFor(Family.all(Physical.class, Objective.class).get());
+        objectives = engine.getEntitiesFor(Family.all(Objective.class).get());
         players = engine.getEntitiesFor(Family.all(Player.class).get());
     }
 
     // Every frame the objectives system checks if the objective components has had a player component collide with it
     public void update(float deltaTime) {
-        for (int i = 0; i < objectives.size(); i++) {
-            Entity objective = objectives.get(i);
-            Objective area = objectiveMapper.get(objective);
-            for (int j = 0; j < players.size(); j++) {
-                Entity player = players.get(j);
-                Vector2 position = physicalMapper.get(player).getPosition();
-                if (area.contains(position)) {
-                    playerMapper.get(player).complete();
+        for (Entity objectiveEntity : objectives) {
+            Objective objective = objectiveMapper.get(objectiveEntity);
+            for (Entity playerEntity : players) {
+                Vector2 playerPosition = physicalMapper.get(playerEntity).getPosition();
+                Player player = playerMapper.get(playerEntity);
+                if (objective.contains(playerPosition) && !player.isCompleted()) {
+                    player.complete();
                 }
             }
 
