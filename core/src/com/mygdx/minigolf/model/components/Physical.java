e85cfa741314b84111e871abb3e6d35c96da9f49
Kristian <kristianint@outlook.com>
2021-03-23 11:23:31 +0100

    add more getters and setters to Physical

--- core/src/com/mygdx/minigolf/model/components/Physical.java
+++ core/src/com/mygdx/minigolf/model/components/Physical.java
@@ -1,7 +1,11 @@
 package com.mygdx.minigolf.model.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Shape;
 
 public class Physical implements Component {
 
@@ -10,6 +14,18 @@ public class Physical implements Component {
 
     public Physical(Body body) {
         this.body = body;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public Fixture getFixture() {
+        return this.body.getFixtureList().get(0);
     }
 
     public boolean isCollidable() {
@@ -21,7 +37,7 @@ public class Physical implements Component {
     }
 
     public float getBounce() {
-        return this.body.getFixtureList().get(0).getRestitution();
+        return this.getFixture().getRestitution();
     }
 
     /**
@@ -32,15 +48,47 @@ public class Physical implements Component {
      * @param bounce Float from 0 to 1.
      */
     public void setBounce(float bounce) {
-        this.body.getFixtureList().get(0).setRestitution(bounce);
+        this.getFixture().setRestitution(bounce);
     }
 
-    public Body getBody() {
-        return body;
+    public Vector2 getVelocity() {
+        return this.body.getLinearVelocity();
     }
 
-    public void setBody(Body body) {
-        this.body = body;
+    public void setVelocity(Vector2 velocity) {
+        this.body.setLinearVelocity(velocity);
+    }
+
+    public Vector2 getPosition() {
+        return this.body.getPosition();
+    }
+
+    public void setPosition(Vector2 position) {
+        this.body.getPosition().set(position.x, position.y);
+    }
+
+    public Shape getShape() {
+        return this.getFixture().getShape();
+    }
+
+    public BodyDef.BodyType getType() {
+        return body.getType();
+    }
+
+    public float getFriction() {
+        return this.getFixture().getFriction();
+    }
+
+    public void setFriction(float friction) {
+        this.getFixture().setFriction(friction);
+    }
+
+    public float getDensity() {
+        return this.getFixture().getDensity();
+    }
+
+    public void setDensity(float density) {
+        this.getFixture().setDensity(density);
     }
 
 }
