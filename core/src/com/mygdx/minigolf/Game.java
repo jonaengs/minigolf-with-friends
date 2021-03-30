0ad747ba02c4391f17cc6695cbccb9c791120bf3
aleksawk <aleksawk@stud.ntnu.no>
2021-03-30 11:29:39 +0200

    #26 fix merge

--- core/src/com/mygdx/minigolf/Game.java
+++ core/src/com/mygdx/minigolf/Game.java
@@ -1,52 +1,84 @@
 package com.mygdx.minigolf;
 
-import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.minigolf.controller.EntityFactory;
 import com.mygdx.minigolf.controller.systems.GraphicsSystem;
-import com.mygdx.minigolf.model.components.Graphical;
+import com.mygdx.minigolf.controller.systems.Physics;
 import com.mygdx.minigolf.model.components.Physical;
 
 public class Game extends ApplicationAdapter {
 
-    private World world;
+    Engine engine;
+    World world;
+    EntityFactory factory;
     public static final Vector2 spawnPosition = new Vector2(0,0);
-
-    private SpriteBatch batch;
-    private Engine engine;
-    private EntityFactory entityFactory;
-
-    private final ComponentMapper<Physical> physicalMapper = ComponentMapper.getFor(Physical.class);
-    private final ComponentMapper<Graphical> graphicalMapper = ComponentMapper.getFor(Graphical.class);
 
     @Override
     public void create() {
-        world = new World(new Vector2(0, 0), true);
-
-        batch = new SpriteBatch();
-
-        GraphicsSystem graphicsSystem = new GraphicsSystem(batch);
-
         engine = new Engine();
+        world = new World(new Vector2(0, -10), true);
+
+        GraphicsSystem graphicsSystem = new GraphicsSystem();
+
         engine.addSystem(graphicsSystem);
+        engine.addSystem(new Physics(world, engine));
+        // engine.addSystem(new PhysicsDebugSystem(world, graphicsSystem.getCam()));
 
-        entityFactory = new EntityFactory(engine);
+        factory = new EntityFactory(engine, world);
 
-        // Create a golf ball and obstacle for testing purposes
-        createGolfBall();
-        createObstacle();
+        // --- Start dummy demo code ---
+        factory.createPlayer(9, 12, false);
+
+        Vector2[] triangle = new Vector2[]{
+                new Vector2(0, 0),
+                new Vector2(2, 0),
+                new Vector2(2, 1),
+        };
+
+        // demo highest priority wins
+        Entity e1 = factory.createObstacle(8, 1, triangle);
+        // added first, but highest priority, will be executed last and have final say
+        e1.getComponent(Physical.class).addContactListener(new Physical.ContactListener(100) {
+            @Override
+            public void ignoreContact(Entity other, Contact contact) {
+                contact.setEnabled(true);
+            }
+        });
+        // added second, but lower priority, will be executed first and later listeners might
+        // override decisions this listener caused
+        e1.getComponent(Physical.class).addContactListener(new Physical.ContactListener(10) {
+            @Override
+            public void ignoreContact(Entity other, Contact contact) {
+                contact.setEnabled(false);
+            }
+        });
+
+        // demo disable contact; fall through entity
+        factory.createObstacle(8, 4, triangle).getComponent(Physical.class).addContactListener(new Physical.ContactListener(1) {
+            @Override
+            public void ignoreContact(Entity other, Contact contact) {
+                contact.setEnabled(false);
+            }
+        });
+
+        // demo removing entity on contact (e.g. power-up)
+        Entity e3 = factory.createObstacle(8, 7, triangle);
+        e3.getComponent(Physical.class).addContactListener(new Physical.ContactListener(1) {
+            @Override
+            public void ignoreContact(Entity other, Contact contact) {
+                engine.removeEntity(e3);
+                contact.setEnabled(false);
+            }
+        });
+        // --- End dummy demo code ---
+
     }
 
     @Override
@@ -54,66 +86,10 @@ public class Game extends ApplicationAdapter {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        engine.update(Gdx.graphics.getDeltaTime());
-
-        world.step(1 / 60f, 6, 2);
+        engine.update(Gdx.graphics.getDeltaTime()); // TODO: Move stuff to GameView
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        world.dispose();
-    }
-
-    private void createGolfBall() {
-        Entity golfBall = entityFactory.createEntity(EntityFactory.EntityType.GOLFBALL);
-
-        Physical physical = physicalMapper.get(golfBall);
-
-        // Create body for ball
-        BodyDef ballBodyDef = new BodyDef();
-        ballBodyDef.type = BodyDef.BodyType.DynamicBody;
-        ballBodyDef.position.set(1, 1);
-
-        Body ballBody = world.createBody(ballBodyDef);
-        CircleShape circle = new CircleShape();
-        circle.setRadius(0.15f);
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = circle;
-        ballBody.createFixture(fixtureDef);
-        circle.dispose();
-
-        // Add ball body to physical component
-        physical.setBody(ballBody);
-
-        Graphical graphical = graphicalMapper.get(golfBall);
-        graphical.setLayer(1);
-    }
-
-    private void createObstacle() {
-        Entity obstacle = entityFactory.createEntity(EntityFactory.EntityType.OBSTACLE);
-
-        Physical physical = physicalMapper.get(obstacle);
-
-        // Create body for obstacle
-        BodyDef obstacleBodyDef = new BodyDef();
-        obstacleBodyDef.type = BodyDef.BodyType.StaticBody;
-        obstacleBodyDef.position.set(2, 4);
-
-        Body obstacleBody = world.createBody(obstacleBodyDef);
-        PolygonShape poly = new PolygonShape();
-        poly.setAsBox(3f / 2, 2f / 2);
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = poly;
-        obstacleBody.createFixture(fixtureDef);
-        poly.dispose();
-
-        // Add obstacle body to physical component
-        physical.setBody(obstacleBody);
-
-        Graphical graphical = graphicalMapper.get(obstacle);
-        graphical.setLayer(1);
-        graphical.setWidth(3);
-        graphical.setHeight(2);
     }
 }
--- core/src/com/mygdx/minigolf/model/components/Physical.java
+++ core/src/com/mygdx/minigolf/model/components/Physical.java
@@ -1,29 +1,53 @@
 package com.mygdx.minigolf.model.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Shape;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Physical implements Component {
 
     private Body body;
+    private List<ContactListener> contactListeners = new ArrayList<>();
+
+    public Physical(Body body) {
+        this.body = body;
+    }
+
+    public Body getBody() {
+        return body;
+    }
 
     public void setBody(Body body) {
         this.body = body;
     }
 
-    public Body getBody() {
-        return this.body;
+    public Fixture getFixture() {
+        return this.body.getFixtureList().get(0);
     }
 
-    public Vector2 getPosition() {
-        return body.getPosition();
+    public float getBounce() {
+        return this.getFixture().getRestitution();
     }
 
-    public void setPosition(Vector2 position) {
-        body.getPosition().set(position.x, position.y);
+    /**
+     * Set bounce [0, 1].<br>
+     * 1 => 100% bounce, would never stop bouncing on a flat surface.<br>
+     * 0 => No bounce.
+     *
+     * @param bounce Float from 0 to 1.
+     */
+    public void setBounce(float bounce) {
+        this.getFixture().setRestitution(bounce);
     }
 
     public Vector2 getVelocity() {
@@ -34,11 +58,70 @@ public class Physical implements Component {
         this.body.setLinearVelocity(velocity);
     }
 
-    public Shape getShape() {
-        return this.body.getFixtureList().get(0).getShape();
+    public Vector2 getPosition() {
+        return this.body.getPosition();
     }
 
-    public BodyType getBodyType() {
-        return this.body.getType();
+    public void setPosition(Vector2 position) {
+        this.body.getPosition().set(position.x, position.y);
     }
+
+    public Shape getShape() {
+        return this.getFixture().getShape();
+    }
+
+    public BodyDef.BodyType getType() {
+        return body.getType();
+    }
+
+    public float getFriction() {
+        return this.getFixture().getFriction();
+    }
+
+    public void setFriction(float friction) {
+        this.getFixture().setFriction(friction);
+    }
+
+    public float getDensity() {
+        return this.getFixture().getDensity();
+    }
+
+    public void setDensity(float density) {
+        this.getFixture().setDensity(density);
+    }
+
+    public void addContactListener(ContactListener listener) {
+        contactListeners.add(listener);
+        contactListeners.sort(Comparator.comparingInt(a -> a.priority));
+    }
+
+    public boolean removeContactListener(ContactListener listener) {
+        return contactListeners.remove(listener);
+    }
+
+    public List<ContactListener> getContactListeners() {
+        return Collections.unmodifiableList(contactListeners);
+    }
+
+    public static class ContactListener {
+
+        public final int priority;
+
+        public ContactListener(int priority) {
+            this.priority = priority;
+        }
+
+        public void beginContact(Entity other, Contact contact) {
+        }
+
+        public void endContact(Entity other, Contact contact) {
+        }
+
+        /**
+         * Called before contact happens. Set contact. Set <code>contact.setEnabled(false);</code> to ignore this contact.
+         */
+        public void ignoreContact(Entity other, Contact contact) {
+        }
+    }
+
 }
