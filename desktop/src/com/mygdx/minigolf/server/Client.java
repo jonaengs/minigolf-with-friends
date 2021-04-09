7a4247c58444bdf0a8fb5351c32c28d433347191
jonatan <jonaengs@gmail.com>
2021-04-09 23:09:53 +0200

    start setting up game in client code

--- desktop/src/com/mygdx/minigolf/server/Client.java
+++ desktop/src/com/mygdx/minigolf/server/Client.java
@@ -1,7 +1,10 @@
 package com.mygdx.minigolf.server;
 
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.mygdx.minigolf.Game;
+import com.mygdx.minigolf.model.components.Physical;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,7 +13,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PushbackInputStream;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 class Client {
     static final String[] names = {"Hannah", "Ludo", "Bathilda", "Katie", "Cuthbert", "Phineas", "Sirius", "Amelia", "Susan", "Terry", "Lavender", "Millicent", "Charity", "Frank", "Alecto", "Amycus", "Reginald", "Mary", "Cho", "Penelope", "Michael", "Vincent", "Vincent", "Colin", "Dennis", "Dirk", "Bartemius", "Bartemius", "Roger", "Dawlish", "Fleur", "Gabrielle", "Dedalus", "Amos", "Cedric", "Elphias", "Antonin", "Aberforth", "Albus", "Dudley", "Marjorie", "Petunia", "Vernon", "Marietta", "Arabella", "Argus", "Justin", "Seamus", "Marcus", "Mundungus", "Filius", "Florean", "Cornelius", "Marvolo", "Merope", "Morfin", "Anthony", "Goyle", "Gregory", "Hermione", "Astoria", "Gregorovitch", "Fenrir", "Gellert", "Wilhelmina", "Godric", "Rubeus", "Madam", "Mafalda", "Helga", "Lee", "Bertha", "Igor", "Viktor", "Bellatrix", "Rabastan", "Rodolphus", "Gilderoy", "Alice", "Augusta", "Frank", "Neville", "Luna", "Xenophilius", "Remus", "Edward", "Walden", "Draco", "Lucius", "Narcissa", "Scorpius", "Madam", "Griselda", "Madam", "Olympe", "Ernie", "Minerva", "Cormac", "Graham", "Alastor", "Auntie", "Theodore", "Bob", "Garrick", "Pansy", "Padma", "Parvati", "Peter", "Antioch", "Cadmus", "Ignotus", "Irma", "Sturgis", "Poppy", "Harry", "James", "Lily", "Quirinus", "Helena", "Rowena", "Tom", "Demelza", "Augustus", "Albert", "Newt", "Rufus", "Kingsley", "Stanley", "Aurora", "Rita", "Horace", "Salazar", "Hepzibah", "Zacharias", "Severus", "Alicia", "Pomona", "Pius", "Dean", "Andromeda", "Nymphadora", "Ted", "Travers", "Sybill", "Wilky", "Dolores", "Emmeline", "Romilda", "Septima", "Lord", "Angelina", "Myrtle", "Arthur", "Bill", "Charlie", "Fred", "George", "Ginny", "Hugo", "Molly", "Percy", "Ron", "Rose", "Oliver", "Yaxley", "Blaise"};
@@ -70,6 +77,8 @@ class Client {
 
     public void runAsThread() {
         new Thread(() -> {
+            Game game;
+            String[] playerList = new String[]{name};
             Thread.currentThread().setName(this.getClass().getName() + "-" + name);
             while (true) {
                 String msg;
@@ -79,12 +88,39 @@ class Client {
                     }
                     msg = recv();
                     if (msg != null && msg.contentEquals("ENTER GAME")) {
+                        game = new Game();
+                        new LwjglApplication(game);
+                        Game finalGame = game;
+                        Thread.sleep(1000); // Sleep to allow create method to run
+
+                        Map<String, Entity> players = Arrays.stream(playerList)
+                                .collect(Collectors.toMap(
+                                        player -> player,
+                                        player -> finalGame.getFactory().createPlayer(0, 0, player.contentEquals(name))
+                                ));
+                        Map<String, Physical> playerPhysicalComponents = players.keySet().stream()
+                                .collect(Collectors.toMap(
+                                        comm -> comm,
+                                        comm -> players.get(comm).getComponent(Physical.class)
+                                ));
+
                         send("GAME READY");
                     }
                     if (msg != null && msg.contentEquals("START GAME")) {
                         Random r = new Random();
                         Thread.sleep(r.nextInt(3_000));
-                        send(r.nextFloat() + ", " + r.nextFloat());
+                        send(5 * r.nextGaussian() + ", " + 5 * r.nextFloat());
+                        // Assume names are unique. TODO: set names server-side and send them to clients when they join a lobby
+                        while (true) {
+                            String data = recv();
+                            Arrays.stream(data.split("/")).forEach(info -> {
+                                String[] split = info.split(", ");
+
+                            });
+                        }
+                    }
+                    else {
+                        playerList = msg.split(", ");
                     }
                 } catch (IOException e) {
                     break;
