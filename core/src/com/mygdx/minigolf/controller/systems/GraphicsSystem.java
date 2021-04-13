9bc640ecd46f93c25a6510e26d96f0bca1957258
Runar Sæther <runarsather@hotmail.com>
2021-04-13 17:00:33 +0200

    Add FitViewport and change screen orientation.

--- core/src/com/mygdx/minigolf/controller/systems/GraphicsSystem.java
+++ core/src/com/mygdx/minigolf/controller/systems/GraphicsSystem.java
@@ -3,17 +3,18 @@ package com.mygdx.minigolf.controller.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.SortedIteratingSystem;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.minigolf.controller.ComponentMappers.GraphicalMapper;
 import com.mygdx.minigolf.controller.ComponentMappers.PhysicalMapper;
 import com.mygdx.minigolf.controller.LayerComparator;
 import com.mygdx.minigolf.model.components.Graphical;
 import com.mygdx.minigolf.model.components.Physical;
+import com.mygdx.minigolf.model.levels.CourseLoader;
 
 import java.util.Comparator;
 
@@ -23,10 +24,11 @@ public class GraphicsSystem extends SortedIteratingSystem {
     public static final float PPM = 32f;
 
     // Height and width of camera frustum, based off width and height of the screen and pixel per meter ratio
-    public static final float FRUSTUM_WIDTH = Gdx.graphics.getWidth() / PPM;
-    public static final float FRUSTUM_HEIGHT = Gdx.graphics.getHeight() / PPM;
+    public static final float FRUSTUM_WIDTH = CourseLoader.SCREEN_WIDTH / PPM;
+    public static final float FRUSTUM_HEIGHT = CourseLoader.SCREEN_HEIGHT / PPM;
 
     private final OrthographicCamera cam = new OrthographicCamera(FRUSTUM_WIDTH, FRUSTUM_HEIGHT);
+    private final FitViewport viewport;
 
     private final Array<Entity> renderQueue = new Array<>(); // A sorted array of entities based on level
     private final Comparator<Entity> comparator = new com.mygdx.minigolf.controller.LayerComparator(); // A comparator to sort entities based on their level
@@ -39,6 +41,7 @@ public class GraphicsSystem extends SortedIteratingSystem {
         super(Family.all(Physical.class, Graphical.class).get(), new LayerComparator());
 
         cam.position.set(FRUSTUM_WIDTH / 2f, FRUSTUM_HEIGHT / 2f, 0);
+        viewport = new FitViewport(FRUSTUM_WIDTH, FRUSTUM_HEIGHT, cam);
     }
 
     @Override
@@ -82,5 +85,9 @@ public class GraphicsSystem extends SortedIteratingSystem {
 
     public OrthographicCamera getCam() {
         return cam;
+    }
+
+    public FitViewport getViewport() {
+        return viewport;
     }
 }
